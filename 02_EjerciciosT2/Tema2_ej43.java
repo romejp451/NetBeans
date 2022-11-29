9ad3446aa0875e29278aa9a0661ca3b822a5c642
Romeo <114758604+romejp451@users.noreply.github.com>
2022-11-29 17:24:04 +0100

    Add files via upload

---
+++ 02_EjerciciosT2/Tema2_ej43.java
@@ -0,0 +1,18 @@
+package tema2_ej43;
+
+import static java.lang.Math.atan;
+
+/**
+ *
+ * @author Romeo <romejp451@gmail.com>
+ */
+public class Tema2_ej43 {
+    public static void main(String[] args) {
+        System.out.println("Calculamos 𝜋 = 4 ∗ 𝑎𝑟𝑐 𝑡𝑔(1) y es :");
+        double arc;
+        arc = atan(1);
+        System.out.println("𝜋 vale: "+4*arc);
+        
+    }
+    
+}
